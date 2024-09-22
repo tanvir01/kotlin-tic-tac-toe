a69fead3167cb584368211e93fd58505ef851396
Tanvir Mahmud Rabbi <tanvirrabbi0@gmail.com>
2024-09-22 13:10:06 +0200

    refactoring v2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,9 +6,17 @@ fun main() {
     println("Welcome to TIC-TAC-TOE game!")
     println("Pick a number from 0-8")
 
-    // take user input
-    println("Enter your name:")
-    val name = readLine()
+    // take user input for name
+    var name: String?
+    while (true) {
+        println("Enter your name:")
+        name = readlnOrNull()
+        if (name.isNullOrEmpty()) {
+            println("Invalid name")
+            continue
+        }
+        break
+    }
 
     // create 2 Players
     val player1 = Player(name.toString(), 'X')
@@ -20,8 +28,8 @@ fun main() {
     game.startGame()
     if (game.isGameEnded()) {
         println("Do you want to play again? (yes/no)")
-        val playAgain = readLine()
-        if (playAgain == "yes") {
+        val playAgain = readlnOrNull() ?: "no"
+        if (playAgain.equals("yes", ignoreCase = true)) {
             main()
         }
     }
